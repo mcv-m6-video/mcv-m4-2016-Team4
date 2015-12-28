318496d359da712c774fa77607fa425b883c60e2
Guim <guimperarnau@gmail.com>
2015-12-28 15:34:44 +0100

    Color addition Task 1.

--- block2/src/oneGaussianBackground.m
+++ block2/src/oneGaussianBackground.m
@@ -1,4 +1,4 @@
-function [mu, sigma] = oneGaussianBackground( sequence , folderPath , fileFormat , pathResults , alpha )
+function [mu, sigma] = oneGaussianBackground( sequence , folderPath , fileFormat , pathResults , alpha , colorIm , colorTransform )
 %ONEGAUSSIANBACKGROUND Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,26 +6,61 @@ function [mu, sigma] = oneGaussianBackground( sequence , folderPath , fileFormat
         alpha = 2.5;
     end
     
+    % check if we want to use colour
+    if ~exist('colorTransform', 'var')
+        colorTransform = @(x) x;
+    end
+    
+    if ~exist('colorIm', 'var')
+        colorIm = false;
+    end
+    
     % First 50% of the test sequence to model the background
-    cumpixel = [];
+    if colorIm
+        cumpixel = cell(1,3);
+    else
+        cumpixel = cell(1,1);
+    end
+    
     for i = 1:floor(length(sequence)/2)
         imName = sprintf('%06d', sequence(i));
         fileName = [ folderPath , imName , fileFormat ];
-        im = double( rgb2gray( imread(fileName) ) );
-        cumpixel = cat(3 , cumpixel , im );
+        im = imread(fileName);
+        if ~colorIm
+            im = rgb2gray( im );
+        else
+            im = colorTransform( im );
+        end
+        im = double( im );
+        for j = 1:size(im,3)
+            cumpixel{j} = cat(3 , cumpixel{j} , im(:,:,j) );
+        end
     end % for
     
-    mu = mean(cumpixel , 3);
-    sigma = std(cumpixel , 0 , 3);
+    muAux = cellfun(@(x) mean(x,3), cumpixel, 'UniformOutput', false);
+    sigmaAux = cellfun(@(x) std(x , 0 , 3), cumpixel, 'UniformOutput', false);
+    
+    mu = []; sigma = [];
+    for i = 1:length(muAux)
+        mu = cat(3 , mu , muAux{i} );
+        sigma = cat(3 , sigma , sigmaAux{i} );
+    end
     
     % Prevent low values of sigma
     sigma = sigma + 2;
+    
     % Second 50% to segment the foreground
     for i = (floor(length(sequence)/2)+1):length(sequence)
         imName = sprintf('%06d', sequence(i));
         fileName = [ folderPath , imName , fileFormat ];
-        im = double( rgb2gray( imread(fileName) ) );
-        background = ~(abs(im - mu) < (alpha*sigma));
+        im = imread(fileName);
+        if ~colorIm
+            im = rgb2gray( im );
+        else
+            im = colorTransform( im );
+        end
+        im = double( im );
+        background = ~(prod(abs(im - mu),3) < prod(alpha*sigma,3));
         imwrite(background , [ pathResults , imName , '.png' ] )
     end % for
     
