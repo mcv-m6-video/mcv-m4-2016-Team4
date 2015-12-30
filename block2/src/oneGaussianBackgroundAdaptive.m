3f28e587f0ecbdca62d64ca28c060623a4a29975
Pau Riba Fiérrez <ragnos.91@gmail.com>
2015-12-30 12:44:37 +0100

    Color for the Recursive gaussian

--- block2/src/oneGaussianBackgroundAdaptive.m
+++ block2/src/oneGaussianBackgroundAdaptive.m
@@ -1,4 +1,4 @@
-function [ ] = oneGaussianBackgroundAdaptive( sequence , folderPath , fileFormat , pathResults , alpha, rho)
+function [ ] = oneGaussianBackgroundAdaptive( sequence , folderPath , fileFormat , pathResults , alpha, rho , colorIm , colorTransform)
 %ONEGAUSSIANBACKGROUND Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,17 +6,46 @@ function [ ] = oneGaussianBackgroundAdaptive( sequence , folderPath , fileFormat
         alpha = 2.5;
     end
     
+    % check if we want to use colour
+    if ~exist('colorTransform', 'var')
+        colorTransform = @(x) x;
+    end
+    if ~exist('colorIm', 'var')
+        colorIm = false;
+    end
+    
     % First 50% of the test sequence to model the background
-    cumpixel = [];
+    if colorIm
+        cumpixel = cell(1,3);
+    else
+        cumpixel = cell(1,1);
+    end
+    
     for i = 1:floor(length(sequence)/2)
         imName = sprintf('%06d', sequence(i));
         fileName = [ folderPath , imName , fileFormat ];
-        im = double( rgb2gray( imread(fileName) ) );
-        cumpixel = cat(3 , cumpixel , im );
+        im = imread( fileName );
+        if ~colorIm
+            im = rgb2gray( im );
+        else
+            im = colorTransform( im );
+        end
+        im = double( im );
+        for j = 1:size(im,3)
+            cumpixel{j} = cat(3 , cumpixel{j} , im(:,:,j) );
+        end
     end % for
     
-    mu = mean(cumpixel , 3);
-    sigma_square = var(cumpixel , 0 , 3);
+    muAux = cellfun(@(x) mean(x,3), cumpixel, 'UniformOutput', false);
+    sigmaAux = cellfun(@(x) var(x , 0 , 3), cumpixel, 'UniformOutput', false);
+    
+    % Conversion from cell to matrix
+    mu = []; sigma_square = [];
+    for i = 1:length(muAux)
+        mu = cat(3 , mu , muAux{i} );
+        sigma_square = cat(3 , sigma_square , sigmaAux{i} );
+    end
+    
     % Second 50% to segment the foreground
     for i = (floor(length(sequence)/2)+1):length(sequence)
         % Adapt the mu and sigma
@@ -27,10 +56,21 @@ function [ ] = oneGaussianBackgroundAdaptive( sequence , folderPath , fileFormat
         
         imName = sprintf('%06d', sequence(i));
         fileName = [ folderPath , imName , fileFormat ];
-        im = double( rgb2gray( imread(fileName) ) );
+        im = imread( fileName );
+        if ~colorIm
+            im = rgb2gray( im );
+        else
+            im = colorTransform( im );
+        end
+        im = double( im );
+        % background --> 0      foreground --> 1
         background = ~(abs(im - mu) < (alpha*sigma_plus2));
-              
-        applyRho = (~background).*rho;
+                
+        % In case of color images, if any dimension falls outside the
+        % gaussian, it will be considered as foreground
+        background = any(background,3);
+        
+        applyRho = repmat( (~background).*rho,1,1,size(im,3) );
         
         mu = (1-applyRho).*mu + applyRho.*im;
         sigma_square = (1-applyRho).*sigma_square + applyRho.*((im - mu).^2);
