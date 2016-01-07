f62e74b6209ba5c16dbf18b9a92398967a630057
Guim <guimperarnau@gmail.com>
2016-01-08 00:23:50 +0100

    Setup file organized with a struct. This way the different sequences will be iterable. Created a code that saves the best result from B2 (obtainBestResultsB2.m) that will be used as baseline to improve.

---
+++ block3/src/setup.m
@@ -0,0 +1,37 @@
+%%% SETUP ENVIRONMENT %%%
+
+%% General parameters
+verbose = false;
+addpath('../../src/backgroundSegmentation');
+addpath('../../src/evaluation');
+
+%% General configuration of the model (common for each sequence)
+colorIm = true;
+colorTransform = @rgb2lab;
+pathDatasets = ['..' filesep '..' filesep 'datasets' filesep];
+folderInput = [ 'input' filesep 'in' ];
+folderGroundtruth = [ 'groundtruth' filesep 'gt' ];
+folderFigures = ['..' filesep 'figures' filesep];
+% Resultng masks of B2 that will be taken as baseline to improve.
+folderBaseResults = ['bestResultsB2' filesep];
+fileFormat = '.jpg';
+
+%% Specific configuration for every sequence
+% Order followed: Highway, Fall, Traffic
+seq.basePaths = {[pathDatasets 'highway' filesep];...
+                 [pathDatasets 'fall' filesep]; ...
+                 [pathDatasets 'traffic' filesep]};
+seq.nSequences = size(seq.basePaths,1);
+% Where the input raw images are stored.
+seq.inputFolders = {[seq.basePaths{1} folderInput];...
+                   [seq.basePaths{2} folderInput];...
+                   [seq.basePaths{3} folderInput];};
+% Where the groundtruth of those raw images is stored.
+seq.gtFolders = {[seq.basePaths{1} folderGroundtruth];...
+                 [seq.basePaths{2} folderGroundtruth];...
+                 [seq.basePaths{3} folderGroundtruth];};
+% Specific frame indexs that are going to be analyzed.
+seq.framesInd = {1050:1350; 1460:1560; 950:1050};
+% Best Rhos and Alphas for every sequence (1G Recursive)
+seq.rhos = [0.2, 0.1, 0.2];
+seq.alphas = [3, 3, 4];
---
+++ src/backgroundSegmentation/oneGaussianBackgroundAdaptive.m
@@ -0,0 +1,82 @@
+function [ ] = oneGaussianBackgroundAdaptive( sequence , folderPath , fileFormat , pathResults , alpha, rho , colorIm , colorTransform)
+%ONEGAUSSIANBACKGROUND Summary of this function goes here
+%   Detailed explanation goes here
+
+    if ~exist('alpha', 'var')
+        alpha = 2.5;
+    end
+    
+    % check if we want to use colour
+    if ~exist('colorTransform', 'var')
+        colorTransform = @(x) x;
+    end
+    if ~exist('colorIm', 'var')
+        colorIm = false;
+    end
+    
+    % First 50% of the test sequence to model the background
+    if colorIm
+        cumpixel = cell(1,3);
+    else
+        cumpixel = cell(1,1);
+    end
+    
+    for i = 1:floor(length(sequence)/2)
+        imName = sprintf('%06d', sequence(i));
+        fileName = [ folderPath , imName , fileFormat ];
+        im = imread( fileName );
+        if ~colorIm
+            im = rgb2gray( im );
+        else
+            im = colorTransform( im );
+        end
+        im = double( im );
+        for j = 1:size(im,3)
+            cumpixel{j} = cat(3 , cumpixel{j} , im(:,:,j) );
+        end
+    end % for
+    
+    muAux = cellfun(@(x) mean(x,3), cumpixel, 'UniformOutput', false);
+    sigmaAux = cellfun(@(x) var(x , 0 , 3), cumpixel, 'UniformOutput', false);
+    
+    % Conversion from cell to matrix
+    mu = []; sigma_square = [];
+    for i = 1:length(muAux)
+        mu = cat(3 , mu , muAux{i} );
+        sigma_square = cat(3 , sigma_square , sigmaAux{i} );
+    end
+    
+    % Second 50% to segment the foreground
+    for i = (floor(length(sequence)/2)+1):length(sequence)
+        % Adapt the mu and sigma
+        sigma = sqrt(sigma_square);
+        % Prevent low values of sigma
+        sigma_plus2 = sigma + 2;
+        
+        
+        imName = sprintf('%06d', sequence(i));
+        fileName = [ folderPath , imName , fileFormat ];
+        im = imread( fileName );
+        if ~colorIm
+            im = rgb2gray( im );
+        else
+            im = colorTransform( im );
+        end
+        im = double( im );
+        % background --> 0      foreground --> 1
+        background = ~(abs(im - mu) < (alpha*sigma_plus2));
+                
+        % In case of color images, if any dimension falls outside the
+        % gaussian, it will be considered as foreground
+        background = any(background,3);
+        
+        applyRho = repmat( (~background).*rho,[1,1,size(im,3)] );
+        
+        mu = (1-applyRho).*mu + applyRho.*im;
+        sigma_square = (1-applyRho).*sigma_square + applyRho.*((im - mu).^2);
+        
+        imwrite(background , [ pathResults , imName , '.png' ] )
+    end % for
+    
+end % function
+
