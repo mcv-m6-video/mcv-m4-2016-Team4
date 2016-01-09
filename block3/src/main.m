f84dc8993fad8142c10942b469999e597acdff5e
Guim <guimperarnau@gmail.com>
2016-01-09 01:19:41 +0100

    Code structured in functions.

--- block3/src/main.m
+++ block3/src/main.m
@@ -3,9 +3,16 @@
 % This script computes all the tasks that are done for the block 3 of the
 % project.
 % 
-%% Set up enviroment and the best model of the block 2
-obtainBestResultsB2;
+
+%% Initialize global parameters
+setup;
+
+%% Set up enviroment and get the best model from the block 2
+if ~exist([seq.basePaths{1} folderBaseResults ], 'dir')
+    obtainBestResultsB2(seq, folderBaseResults, fileFormat, colorIm, colorTransform);
+end
 
 % Task 1
-task1;
+connectivity = 4;
+task1(connectivity, seq, folderBaseResults)
 
--- block3/src/obtainBestResultsB2.m
+++ block3/src/obtainBestResultsB2.m
@@ -1,18 +1,21 @@
-% The execution of this function will create at each sequence directory
-% a folder named 'bestResultsB2' with the results that gave the best
-% F1Score in B2. 
-% Model used: One Gaussian Recursive
-% Color Space: LAB
-% Rho and alpha: customized for each sequence
+function obtainBestResultsB2(seq, folderBaseResults, fileFormat, colorIm, colorTransform)
 
-% Initialize global parameters
-setup;
+    % The execution of this function will create at each sequence directory
+    % a folder named 'bestResultsB2' with the results that gave the best
+    % F1Score in B2. 
+    % Model used: One Gaussian Recursive
+    % Color Space: LAB
+    % Rho and alpha: customized for each sequence
 
-for i=1:seq.nSequences
-    folderResult = [seq.basePaths{i} folderBaseResults];
-    if ~exist(folderResult , 'dir')
-        mkdir( folderResult );
-    end 
-    oneGaussianBackgroundAdaptive( seq.framesInd{i}, seq.inputFolders{i}, fileFormat ,...
-                    folderResult, seq.alphas(i), seq.rhos(i), colorIm , colorTransform);
+
+
+    for i=1:seq.nSequences
+        folderResult = [seq.basePaths{i} folderBaseResults];
+        if ~exist(folderResult , 'dir')
+            mkdir( folderResult );
+        end 
+        oneGaussianBackgroundAdaptive( seq.framesInd{i}, seq.inputFolders{i}, fileFormat ,...
+                        folderResult, seq.alphas(i), seq.rhos(i), colorIm , colorTransform);
+    end
+
 end
