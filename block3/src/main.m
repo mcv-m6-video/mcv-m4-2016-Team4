2d6cf6286c24235d4ae0d2eff568cfaa1a0f425e
Adrià <adri00796@gmail.com>
2016-01-10 19:48:11 +0100

    Task 4!

--- block3/src/main.m
+++ block3/src/main.m
@@ -28,14 +28,14 @@ end
 % Generate figures and calculate AUC
 results = load(['savedResults' filesep 'dataTask1']);
 legendStr = {'Baseline', 'Connectivity=4', 'Connectivity=8'};
-[AUCsB2, AUCsT1] = calculateAUCs(seq, results, folderFigures, legendStr, taskId);
+[AUCsB2, AUCsT4] = calculateAUCs(seq, results, folderFigures, legendStr, taskId);
 
 % Get best connectivity and metrics
-[maxAUCT1, bestIndTask1] = max(mean(AUCsT1));
-task1BestResults.prec = results.prec2(:,:,bestIndTask1);
-task1BestResults.rec = results.rec2(:,:,bestIndTask1);
-task1BestResults.f1score = results.f1score2(:,:,bestIndTask1);
-bestConnectivity = connectivity(bestIndTask1);
+[maxAUCT4, bestIndTask4] = max(mean(AUCsT4));
+task4BestResults.prec = results.prec2(:,:,bestIndTask4);
+task4BestResults.rec = results.rec2(:,:,bestIndTask4);
+task4BestResults.f1score = results.f1score2(:,:,bestIndTask4);
+bestConnectivity = connectivity(bestIndTask4);
 
 disp(['Task 1 best connectivity is ' num2str(bestConnectivity)]); 
 %% Task 2
@@ -52,9 +52,9 @@ end
 % Generate figures and calculate AUC
 % To compare with task one best result we have to change the baseline
 results = load(['savedResults' filesep 'dataTask2']);
-results.prec1 = task1BestResults.prec;
-results.rec1 = task1BestResults.rec;
-results.f1score1 = task1BestResults.f1score;
+results.prec1 = task4BestResults.prec;
+results.rec1 = task4BestResults.rec;
+results.f1score1 = task4BestResults.f1score;
 
 legendStr = {'Baseline Task1'};
 % The pixels will change depending on the parameters
@@ -74,14 +74,14 @@ disp(['Task 2 best number of pixels is ' num2str(bestPixels)]);
 %% Task 3
 taskId = '3';
 legendStr = {'Baseline'};
-if maxAUCT2>maxAUCT1
+if maxAUCT2>maxAUCT4
     results = load(['savedResults' filesep 'dataTask2']);
     legendStr{end+1} = 'Task2';
     bestInd = bestIndTask2;
 else
     results = load(['savedResults' filesep 'dataTask1']);
     legendStr{end+1} = 'Task1';
-    bestInd = bestIndTask1;
+    bestInd = bestIndTask4;
 end
 results.prec2 = results.prec2(:,:,bestInd);
 results.rec2 = results.rec2(:,:,bestInd);
@@ -90,3 +90,24 @@ results.f1score2 = results.f1score2(:,:,bestInd);
 calculateAUCs(seq, results, folderFigures, legendStr, taskId);
 
 disp(['The best result is obtained with ' num2str(legendStr{2})]);
+
+%% Task 4
+taskId = '4';
+if ~exist(['savedResults' filesep 'dataTask4.mat'], 'file')
+    morphFunction = @(masks,x) applyMorphoTask4(masks, bestPixels, bestConnectivity);
+    evaluateMorpho(seq, fileFormat, alphaValues, [1], morphFunction, colorIm, colorTransform, taskId);
+else
+   disp('Task 4 results found (savedResults/dataTask4.mat). Skipping Task 4...'); 
+end
+
+% Generate figures and calculate AUC
+results = load(['savedResults' filesep 'dataTask4']);
+legendStr = {'Baseline', 'Optional 4'};
+[AUCsB2, AUCsT4] = calculateAUCs(seq, results, folderFigures, legendStr, taskId);
+
+% Get best connectivity and metrics
+[maxAUCT4, bestIndTask4] = max(mean(AUCsT4));
+task4BestResults.prec = results.prec2(:,:,bestIndTask4);
+task4BestResults.rec = results.rec2(:,:,bestIndTask4);
+task4BestResults.f1score = results.f1score2(:,:,bestIndTask4);
+bestConnectivity = connectivity(bestIndTask4);
