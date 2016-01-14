6a6fccbc69b0f69a95873973ee4c85070ba25ed4
Pau Riba Fiérrez <ragnos.91@gmail.com>
2016-01-15 00:42:15 +0100

    Block 4 prepare folder and upload documentation

---
+++ block4/src/main.m
@@ -0,0 +1,19 @@
+%% Main Script
+% M4. Video Analysis Project
+% This script computes all the tasks that are done for the block 4 of the
+% project.
+% 
+
+%% Setup
+
+%% Task 1
+
+%% Task 2
+
+%% Task 3
+
+%% Task 4
+
+%% Task 5
+
+%% Task 6
